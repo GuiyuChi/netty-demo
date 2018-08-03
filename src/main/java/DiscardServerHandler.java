ba98c816f302b5ded5f1eaac8c59a81320918750
guiyu-shenz <1092369395@qq.com>
2018-08-03 14:12:22 +0800

    test1

--- src/main/java/DiscardServerHandler.java
+++ src/main/java/DiscardServerHandler.java
@@ -10,16 +10,9 @@ import io.netty.util.ReferenceCountUtil;
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        ByteBuf in = (ByteBuf) msg;
-        try {
-            while (in.isReadable()) { // (1)
-                System.out.print((char) in.readByte());
-                System.out.flush();
-            }
-        } finally {
-            ReferenceCountUtil.release(msg); // (2)
-        }
+    public void channelRead(ChannelHandlerContext ctx, Object msg){
+        // 抛弃服务器，将接收到的数据丢弃
+        ((ByteBuf)msg).release();
     }
 
 
